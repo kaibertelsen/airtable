ae25680598027d0f6aef4de3847702e6cb174bdc
kaibertelsen <117456051+kaibertelsen@users.noreply.github.com>
2022-12-13 09:50:31 +0100

    Update api-v03.js

--- api-v03.js
+++ api-v03.js
@@ -1,4 +1,4 @@
-src="https://cdn.jsdelivr.net/gh/kaibertelsen/airtable/config-01.js"
+src="https://cdn.jsdelivr.net/gh/kaibertelsen/airtable/config-03.js"
 
 function airtable(baseID,tableIOd,type,indata,funckid,description){
 var url = "https://api.airtable.com/v0/"+baseID+"/"+tableIOd;
