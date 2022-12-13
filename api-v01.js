3ae6b5e88918241f510f660ca9deecf632df5f63
kaibertelsen <117456051+kaibertelsen@users.noreply.github.com>
2022-12-13 09:01:40 +0100

    Create api-v01.js

---
+++ api-v01.js
@@ -0,0 +1,62 @@
+function airtable(baseID,tableIOd,type,indata,funckid,description){
+var url = "https://api.airtable.com/v0/"+baseID+"/"+tableIOd;
+
+if(type=="DELETE"){
+url = url+"/"+indata;
+}else if(type=="GET"){
+  if (description == "retrieve"){
+// hente informasjon basert på Item ID.
+url = url+"/"+indata;
+}else if(description =="view"){
+// hente informasjon basert på view.
+url = url+"?maxRecords=100&view="+indata;
+}else{
+//hente alle i tabellen
+
+
+}
+}
+var xhr = new XMLHttpRequest();
+xhr.open(type, url);
+xhr.setRequestHeader("Accept", "application/json");
+xhr.setRequestHeader("Authorization", "Bearer keyQS7dGr6jekYJyy");
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+    
+      airtablereturn (xhr.status,xhr.responseText,funckid);
+   }};
+
+var data = [];
+
+if(type =="PATCH"){
+  //slik skal input være
+/*`{
+      "id": "recJnWPOfM2k85k1W",
+      "fields": {
+        "Webside": "www.sportspakke.no",
+        "Type skjema": "webflow",
+        "Tid": "2023-10-28T07:18:12.602Z"
+      }
+    }`
+    */
+data = `{
+"records": [`+indata+`]}`;
+}else if(type =="POST"){
+//slik skal input være
+/*`{
+      "fields": {
+        "Webside": "www.sportspakke.no",
+        "Type skjema": "webflow",
+        "Tid": "2023-10-28T07:18:12.602Z"
+      }
+    }`
+    */
+data = `{
+"records": [`+indata+`]}`;
+}else if (type == "GET"){
+
+}
+xhr.send();
+}
